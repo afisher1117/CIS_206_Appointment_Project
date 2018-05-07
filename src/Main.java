e000b4e69958297c07118399eb6913ca537824a8
afisher1117 <0070342@student.vvc.edu>
2018-05-06 18:26:10 -0700

    Added comments to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 /******************************************************************************
- super class named appointment
- subclass called Onetime, Daily, Monthly
-
- an appointment has a description "see dentist" and a date
-
- write method occursOn(int year, int month, int day) that checks whether the
- appointment occurs on that date
-
- fill an array of Appointment objects with a mixture of appointments
-
- Have user enter a date and print all appointments that occursOn that date
-
+ Filename: Fisher_Chapter2_Arithmetic_CIS206.java
+ Author: Adam Fisher
+ Email: 0070342@student.vvc.edu
+ Description: Improve the appointment book program of Exercise P9.21 by having 
+ the user save the appointment data to a file and reload the data from a file. 
+ The saving part is straightforward: Make a method save. Save the type, 
+ description, and date to a file. The loading part is not so easy. First 
+ determine the type of the appointment to be loaded, create an object of that 
+ type, and then call a method to load the data.
 *******************************************************************************/
 import java.io.*;
 
 public class Main
 {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-        AppointmentMaker a = new AppointmentMaker();
-        a.menuPrint();
+        AppointmentMaker a = new AppointmentMaker();                            //Creates new AppointmentMaker object 
+        a.menuPrint();                                                          //Prints menu to begin 
     }
 }
